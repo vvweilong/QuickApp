17c14866a6ebc11d5c662e95fc2b0ef89275e59b
zhuxiaolong <zhuxiaolong@ithro.com.cn>
2019-12-30 22:12:26 +0800

    1

--- sample/src/main/java/com/oo/sample/MainActivity.kt
+++ sample/src/main/java/com/oo/sample/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.oo.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.oo.annotationlib.InitAnnotation
+import com.oo.annotationlib.InitAnnotationProcess
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val temp = Temp()
+        InitAnnotationProcess.bind(temp)
+        test(temp.value)
+    }
+
+    fun test(a:Int){
+        Log.i("aaaa","$a")
+    }
+
+
+    open class Temp{
+        @InitAnnotation(20)
+        var value:Int=0
     }
 }
